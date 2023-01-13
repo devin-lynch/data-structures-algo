6c863a16bc0e2851bed801933cb76a00c6840307
devin-lynch <devin.l.lynch@gmail.com>
2023-01-12 18:49:47 -0600

    Adjust int q 1 for time complexity

--- exercises/int-question-1.ts
+++ exercises/int-question-1.ts
@@ -56,7 +56,23 @@ const array4: string[] = ['z', 'y', 'x'];
 
 function containsCommonItem2(arr1: string[], arr2: string[]) {
     // loop through first array and create object where properties === items in the array
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
+    // console.log(map);
     // loop through second array and check if item in second array exists on created object
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true
+        }
+    }
+    return false
 }
 
 // O(a + b) -- no longer using a nested loop! much more efficient
+
+containsCommonItem2(array1, array2)
