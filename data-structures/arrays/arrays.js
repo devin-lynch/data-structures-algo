9852f3d07675dbd9abab658146fd01fc9418e854
devin-lynch <devin.l.lynch@gmail.com>
2023-01-18 21:00:53 -0600

    Array solutions

--- data-structures/arrays/arrays.js
+++ data-structures/arrays/arrays.js
@@ -41,4 +41,41 @@ newArray["delete"](0);
 newArray.push('are');
 newArray.push('nice');
 newArray["delete"](1);
-console.log(newArray.get(0));
+// console.log(newArray.get(0));
+// in interviews, treat any string question like an array question. strings are simply an array of characters. reverse a string? convert into an array, loop/operation (split method), then return as a string.
+// # ## # ## # ## # ## # ## # ## # ## #
+// Create a function that reverses a string:
+// 'Hi my name is Andrei' should be:
+// 'ierdnA si eman ym iH'
+function reverse(str) {
+    var arr = [];
+    for (var i = 0; i < str.length; i++) {
+        arr.unshift(str[i]);
+        // console.log('looping here', arr);
+    }
+    // let reversed = arr.toString();
+    var reversed = arr.join('');
+    console.log('answer', reversed);
+    return reversed;
+}
+reverse('testing');
+reverse('Hi my name is Andrei');
+function reverse2(str) {
+    // check input -- TypeScript should take care of this
+    if (!str || str.length < 2 || typeof str !== 'string') {
+        return "hmm that's not good";
+    }
+    var backwards = [];
+    var totalItems = str.length - 1;
+    for (var i = totalItems; i > 0; i--) {
+        backwards.push(str[i]);
+    }
+    console.log(backwards);
+    return backwards.join('');
+}
+reverse2('Hi my name is Andrei');
+function reverse3(str) {
+    console.log(str.split('').reverse().join(''));
+    return str.split('').reverse().join(''); // built in js method 'reverse' and 'join'
+}
+reverse3('Does it work?');
--- data-structures/arrays/arrays.ts
+++ data-structures/arrays/arrays.ts
@@ -50,4 +50,54 @@ newArray.delete(0);
 newArray.push('are');
 newArray.push('nice');
 newArray.delete(1);
-console.log(newArray.get(0));
+// console.log(newArray.get(0));
+
+
+// in interviews, treat any string question like an array question. strings are simply an array of characters. reverse a string? convert into an array, loop/operation (split method), then return as a string.
+
+
+// # ## # ## # ## # ## # ## # ## # ## #
+
+// Create a function that reverses a string:
+// 'Hi my name is Andrei' should be:
+// 'ierdnA si eman ym iH'
+
+function reverse(str: string) {
+    let arr: string[] = [];
+    for (let i = 0; i < str.length; i++) {
+        arr.unshift(str[i]);
+        // console.log('looping here', arr);
+    }
+    // let reversed = arr.toString();
+    let reversed = arr.join('');
+    console.log('answer', reversed);
+    return reversed
+}
+
+reverse('testing')
+reverse('Hi my name is Andrei')
+
+function reverse2(str: string) {
+    // check input -- TypeScript should take care of this
+    if (!str || str.length < 2 || typeof str !== 'string') {
+        return "hmm that's not good"
+    }
+
+    const backwards: string[] = [];
+    const totalItems = str.length - 1;
+    for (let i = totalItems; i > 0; i--) {
+        backwards.push(str[i])
+    }
+    console.log(backwards);
+
+    return backwards.join('');
+}
+
+reverse2('Hi my name is Andrei')
+
+function reverse3(str: string) {
+    console.log(str.split('').reverse().join(''));
+    return str.split('').reverse().join('') // built in js method 'reverse' and 'join'
+}
+
+reverse3('Does it work?')
